f3a5912edaa6486996ce6efe63a001e4ec2edbee
lakshya <lakshya10171@gmail.com>
2025-09-12 15:27:55 +0530

    final hashfunction  code implementation

--- src/main/java/com/ds_algorithms/hashing/HashFunction.java
+++ src/main/java/com/ds_algorithms/hashing/HashFunction.java
@@ -131,3 +131,4 @@ class UniversalHash implements HashFunction {
     }
 }
 
+                                                                                                                                       
