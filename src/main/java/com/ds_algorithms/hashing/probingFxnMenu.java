b2a46e9f1d02c70606d7a2a5ee90cce393d703b8
lakshya <lakshya10171@gmail.com>
2025-09-13 18:01:40 +0530

    implementation of probing hashfunction menu

---
+++ src/main/java/com/ds_algorithms/hashing/probingFxnMenu.java
@@ -0,0 +1,21 @@
+package com.ds_algorithms.hashing;
+
+public class probingFxnMenu {
+    
+    // define the methord
+    public probingFunction getProbingFunction(int choice1,int choice2, int tableSize){
+        // define the switch case
+        switch(choice1){
+            case 1:
+                return new LinearProbing();
+            case 2:
+                return new QuadreaticProbing();
+            case 3:
+                HashFunction second = HashFunctionMenu.getHashFunction(choice2,tableSize-1);
+                return new DoubleProbing();
+            default:
+                System.out.println("SORRY!! Invalid choice");
+                return new LinearProbing();           
+        }
+    }
+}
